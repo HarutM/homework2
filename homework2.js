fb43cfb4f69a948efaaa765869c7e28cf7ff450a
Harutyun Mirzakhanyan <harutyun_mirzakhanyan@edu.aua.am>
2018-09-08 16:50:36 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,20 @@
+<script>
+
+const num1 = 25
+const num2 = 10
+const num3 = num1 + num2
+const num4 = (num1 - num2) * num3
+const text1 = "Hello"
+const text2 = "AUA"
+
+if (text1 === "AUA") {
+  console.log("it is wrong!")
+}; if (text1 === "Hello") {
+  console.log("it's correct!!!")
+}; if (text1 + " " + text2 === "Hello AUA") {
+  console.log("Again it's correct")
+};
+console.log(num3*num1)
+console.log(num4)
+
+</script>	
